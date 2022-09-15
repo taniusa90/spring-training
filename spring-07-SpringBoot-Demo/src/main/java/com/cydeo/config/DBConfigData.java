cb135fd6dd3774ad6e5f16ee25c7f7c54749f3c7
taniusa90 <galea_tanea@mail.com>
2022-09-15 15:30:10 -0500

    Spring Boot

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/config/DBConfigData.java
@@ -0,0 +1,19 @@
+package com.cydeo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+@Configuration
+@ConfigurationProperties(prefix ="db" )
+@Data
+public class DBConfigData {
+
+    private String my_username;
+    private String password;
+    private List<String> type;
+
+
+
+}
--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -1,5 +1,7 @@
 package com.cydeo.service;
 
+import com.cydeo.config.AppConfigData;
+import com.cydeo.config.DBConfigData;
 import com.cydeo.entity.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
@@ -11,15 +13,37 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
+    private final AppConfigData appConfigData;
+    private final DBConfigData dbConfigData;
 
-    public CommentService(CommentRepository commentRepository,@Qualifier("Email") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("Email") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData, DBConfigData dbConfigData) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
+        this.dbConfigData = dbConfigData;
     }
 
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
+    }
+
+    public void printConfigData(){
+
+        System.out.println(appConfigData.getUsername());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
+
+
+    }
+
+    public void printDbConfigData(){
+
+        System.out.println(dbConfigData.getMy_username());
+        System.out.println(dbConfigData.getPassword());
+        System.out.println(dbConfigData.getType());
+
+
     }
 
     }
