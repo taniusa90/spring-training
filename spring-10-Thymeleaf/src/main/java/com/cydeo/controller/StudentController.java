d2c65e4fd5895e240f17b4ddd93b9a202c5a54ed
taniusa90 <galea_tanea@mail.com>
2022-09-25 15:05:11 -0500

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
@@ -17,10 +18,16 @@ public class StudentController {
         return "student/register";
     }
 
-
     @RequestMapping("/drop")//localhost:8080/register
     public String drop(){
         return "student/register";
+    }
+
+
+    @RequestMapping("/welcome")//localhost:8080/register
+    public String welcome(@RequestParam String name){
+        System.out.println(name);
+        return "student/welcome";
     }
 
 
