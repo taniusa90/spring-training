6a18f5f0fa16280dc8c999d115dbb78d1e17ddba
taniusa90 <galea_tanea@mail.com>
2022-10-23 09:29:04 -0500

    schema.sql

--- spring-11-Data-TableCreation/src/main/resources/schema.sql
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -1,7 +1,8 @@
-create table EMPLOYEES(
-
-                          id bigint not null,
-                          name varchar(255),
-                          primary key (id)
+drop table if exists employees;
+create table employees
+(
+    id   bigint not null,
+    name varchar(255),
+    primary key (id)
 
 );
