565be2f70617b4705c335117ebb850d0afd9e8c6
taniusa90 <galea_tanea@mail.com>
2022-11-30 04:06:34 -0600

    bootstrap

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/Spring18RestSchoolLabApplication.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/Spring18RestSchoolLabApplication.java
@@ -2,12 +2,15 @@ package com.cydeo;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class Spring18RestSchoolLabApplication {
 
 	public static void main(String[] args) {
