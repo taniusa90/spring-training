56a804543256c8fc836cc6cbef447752c6beffa0
taniusa90 <galea_tanea@mail.com>
2022-11-30 03:56:07 -0600

    Lab

---
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/entity/Course.java
@@ -0,0 +1,25 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "course")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course extends BaseEntity {
+
+    private String courseName;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+}
