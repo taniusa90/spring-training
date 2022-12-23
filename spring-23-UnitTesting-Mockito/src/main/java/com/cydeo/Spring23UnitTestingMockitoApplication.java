858b3de345c13876b72afa837f5f118b772e64c8
taniusa90 <galea_tanea@mail.com>
2022-12-22 19:40:27 -0600

    Mockito

--- spring-23-UnitTesting-Mockito/src/main/java/com/cydeo/Spring23UnitTestingMockitoApplication.java
+++ spring-23-UnitTesting-Mockito/src/main/java/com/cydeo/Spring23UnitTestingMockitoApplication.java
@@ -1,13 +1,21 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring23UnitTestingMockitoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring23UnitTestingMockitoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Spring23UnitTestingMockitoApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
+	}
+
 
 }
